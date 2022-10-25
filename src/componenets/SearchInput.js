4bfc45cf15ed371741c471dc498ffed7806b7bbb
이종상 <helius96@naver.com>
2022-10-25 22:39:08 +0900

    little

---
+++ src/componenets/SearchInput.js
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+import styles from "../css/searchInput.module.css"
+
+function SearchInput({setData}){
+
+    const [text, setText] = useState("");
+    const onChange = (e) => {
+        setText(e.target.value);
+        setData(e.target.value);
+    }
+
+    return(
+        <div>
+            <form>
+                <input type = "text" 
+                placeholder="please type input" 
+                className={styles.littleSearch}
+                onChange = {onChange}
+                value = {text}                
+                />
+            </form>
+        </div>
+    )
+}
+
+export default SearchInput
